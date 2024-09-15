821e0a8aa6332c3af5807c2cd6d0db08e214997a
wanderson <wandersoncoder@gmail.com>
2024-09-15 15:45:21 -0300

    perf: remove unecessary code

--- src/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const port =  process.env.PORT || 3001;
-const app = express()
-
-app.use(express.static(__dirname))
-
-app.listen(port)
